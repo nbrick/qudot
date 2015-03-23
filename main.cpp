dbf10ea1391db3bb1a337518fdd5979750606888
Nicolas Bricknell <nebricks@gmail.com>
2015-03-23 02:53:51 +0000

    Tidy, reorder code, add TODOs

--- main.cpp
+++ main.cpp
@@ -7,51 +7,110 @@
 
 /* PHYSICAL CONSTANTS */
 
-constexpr double e = 1.60217657e-19;  // Coulombs
-constexpr double temp = 4.2;  // Kelvin
+constexpr double e = 1.60217657e-19;   // Coulombs
+constexpr double temp = 4.2;           // Kelvin
 constexpr double k_B = 1.3806488e-23;  // Joules per Kelvin
 
 
 /* USER CONFIGURATION */
 
 constexpr double single_electron_energies[] = {
-    -0.1*e, 0.1*e, 0.1*e, 0.1*e
+    -0.1*e, -0.1*e, 0.1*e, 0.1*e
 };  // Joules
 
-constexpr double gate_capacitance = 1e-19;  // Farads
+constexpr double gate_capacitance =   1e-19;  // Farads
 constexpr double source_capacitance = 1e-18;  // Farads
-constexpr double drain_capacitance = 1e-18;  // Farads
-constexpr double extra_capacitance = 1e-19;  // Farads
+constexpr double drain_capacitance =  1e-18;  // Farads
+constexpr double extra_capacitance =  1e-19;  // Farads
 
 constexpr double source_width = 1;  // arb.
-constexpr double drain_width = 1;  // arb.
+constexpr double drain_width =  1;  // arb.
 
-constexpr double v_g_min = 0;  // Volts
-constexpr double v_g_max = 0;  // Volts
-constexpr int v_g_steps = 1;  // (y-axis resolution)
+constexpr double v_g_min = 0;      // Volts
+constexpr double v_g_max = 0;      // Volts
+constexpr int v_g_steps =  1;      // (y-axis resolution)
 
 constexpr double v_sd_min = -0.1;  // Volts
-constexpr double v_sd_max = 1;  // Volts
-constexpr int v_sd_steps = 20;  // (x-axis resolution)
+constexpr double v_sd_max =  1;    // Volts
+constexpr int v_sd_steps = 20;     // (x-axis resolution)
+
 
 constexpr double source_dos (double energy) {
-    if (energy > 0) {
+    if (energy > 0.0) {
         // Do nothing. (Suppress 'unused' warning.)
     }
     return 1;
 }
+
 constexpr double d_fermi_energy = 0.0*e;  // Joules
 
+
 constexpr double drain_dos (double energy) {
-    if (energy > 0) {
+    if (energy > 0.0) {
         // Do nothing. (Suppress 'unused' warning.)
     }
     return 1;
 }
+
 constexpr double s_fermi_energy = 0.0*e;  // Joules
+
 
 constexpr double evolution_step_size = 1e-1;
 constexpr double convergence_criterion = 1e-10;
+
+
+/* COMPILE-TIME CALCULATIONS */
+
+constexpr double total_capacitance = gate_capacitance + source_capacitance
+                                     + drain_capacitance + extra_capacitance;
+
+
+/* TYPES */
+
+typedef struct {
+    double gate;
+    double sd;  // Source-drain
+} v_pair;
+
+// TODO: typedef double mu_container[occupied][level][occ_num];
+// Should do this. Otherwise there are many redundant mu calculations.
+
+constexpr int n_levels = sizeof(single_electron_energies)
+                         /sizeof(single_electron_energies[0]);
+
+typedef unsigned long cfg;
+constexpr cfg n_configs = 1 << n_levels;  // 2^n_levels
+
+typedef struct {
+    cfg to;  // Row
+    cfg from;  // Column
+    double value;
+} matrix_element;
+
+typedef double mu_spectrum[n_levels];
+typedef mu_spectrum mu_container[n_configs];
+
+typedef double weights[n_configs];
+typedef double ddt_weights[n_configs];
+
+
+/* BINARY REPRESENTATION OF CONFIGURATIONS */
+
+bool occupied (cfg config, int level) {
+    return (config >> level) & 1;
+}
+
+int sum_occupation (cfg config) {
+    int sum = 0;
+    for (int level = 0; level < n_levels; ++level) {
+        sum += (int)occupied(config, level);
+    }
+    return sum;
+}
+
+int flipped_occupation (cfg config, int level) {
+    return config ^ (1 << level);
+}
 
 
 /* THERMODYNAMICS */
@@ -66,48 +125,20 @@ double fermi (double energy, double chem_pot) {
         return 1.0/(exp(exponent) + 1);
 }
 
+
+/* THE PHYSICS */
+
+/* Energy shifts in leads owing to applied bias */
+
 constexpr double s_shift (double energy, double v_sd) {
     return energy - e*v_sd/2.0;
 }
+
 constexpr double d_shift (double energy, double v_sd) {
     return energy + e*v_sd/2.0;
 }
 
-
-/* TYPES AND COMPILE-TIME CALCULATIONS */
-
-typedef struct {
-    double gate;
-    double sd;  // Source-drain
-} v_pair;
-
-// typedef double mu_container[occupied][level][occ_num];
-// Should do this. Otherwise have many redundant mu calculations.
-
-typedef unsigned long cfg;
-
-constexpr int n_levels = sizeof(single_electron_energies)
-                         /sizeof(single_electron_energies[0]);
-
-typedef struct {
-    cfg to;  // Row
-    cfg from;  // Column
-    double value;
-} matrix_element;
-typedef double mu_spectrum[n_levels];
-
-constexpr cfg n_configs = 1 << n_levels;  // 2^n_levels
-
-typedef std::pair<double, double[n_configs]> iw_list[n_levels];
-typedef mu_spectrum mu_container[n_configs];
-typedef double weights[n_configs];
-typedef double ddt_weights[n_configs];
-
-constexpr double total_capacitance = gate_capacitance + source_capacitance
-                                     + drain_capacitance + extra_capacitance;
-
-
-/* TUNNEL RATES */
+/* Tunnel rates */
 
 double in_from_source (double mu, double v_sd) {
     return source_width
@@ -133,45 +164,15 @@ double out_to_drain (double mu, double v_sd) {
            * (1 - fermi(mu, d_shift(d_fermi_energy, v_sd)));
 }
 
-
-/* MISCELLANEOUS FUNCTIONS */
-
-v_pair voltage_pair_from_index (int index) {
-
-    v_pair voltage;
-
-    voltage.gate = v_g_min
-                   + ((index % v_g_steps)
-                     *(double)(v_g_max - v_g_min)/(double)v_g_steps);
-
-    voltage.sd = v_sd_min
-                   + (floor((double)index/(double)v_g_steps))
-                     *(double)(v_sd_max - v_sd_min)/(double)(v_sd_steps);
-
-    return voltage;
-}
-
-
-/* BINARY REPRESENTATION OF CONFIGURATIONS */
-
-bool occupied (cfg config, int level) {
-    return (config >> level) & 1;
-}
-
-
-int sum_occupation (cfg config) {
-    int sum = 0;
-    for (int level = 0; level < n_levels; ++level) {
-        sum += (int)occupied(config, level);
+double current_through_level (double mu, bool occupied, double v_sd) {
+    if (occupied) {
+        return -e*(out_to_drain(mu, v_sd) - out_to_source(mu, v_sd));
+    } else {
+        return -e*(in_from_source(mu, v_sd) - in_from_drain(mu, v_sd));
     }
-    return sum;
 }
 
-
-int flipped_occupation (cfg config, int level) {
-    return config ^ (1 << level);
-}
-
+/* Charging energy (constant interaction) */
 
 double chemical_potential (bool occupied, double single_electron_energy,
                            int n_electrons_on_dot, v_pair voltage) {
@@ -180,9 +181,10 @@ double chemical_potential (bool occupied, double single_electron_energy,
            + ((e/total_capacitance)
              *((n_electrons_on_dot + (occupied ? -1 : 1)*0.5)*e
                 - gate_capacitance*voltage.gate
-                - ((source_capacitance - drain_capacitance)*voltage.sd/2)));
+                - ((source_capacitance - drain_capacitance)*voltage.sd/2.0)));
 }
 
+/* Rate matrix elements (Beenakker rate equations) */
 
 matrix_element diagonal_matrix_element (cfg config, mu_spectrum spectrum,
                                         double v_sd) {
@@ -197,7 +199,6 @@ matrix_element diagonal_matrix_element (cfg config, mu_spectrum spectrum,
     }
     return { config, config, value };
 }
-
 
 matrix_element offdiagonal_matrix_element (cfg to, cfg from, int level,
                                            double mu,
@@ -214,14 +215,26 @@ matrix_element offdiagonal_matrix_element (cfg to, cfg from, int level,
 }
 
 
-double current_through_level (double mu, bool occupied, double v_sd) {
-    if (occupied) {
-        return -e*(out_to_drain(mu, v_sd) - out_to_source(mu, v_sd));
-    } else {
-        return -e*(in_from_source(mu, v_sd) - in_from_drain(mu, v_sd));
-    }
+/* MISCELLANEOUS HELPER */
+
+v_pair voltage_pair_from_index (int index) {
+    // TODO: Have a 'snake' option.
+
+    v_pair voltage;
+
+    voltage.gate = v_g_min
+                   + ((index % v_g_steps)
+                     *(double)(v_g_max - v_g_min)/(double)v_g_steps);
+
+    voltage.sd = v_sd_min
+                   + (floor((double)index/(double)v_g_steps))
+                     *(double)(v_sd_max - v_sd_min)/(double)(v_sd_steps);
+
+    return voltage;
 }
 
+
+/* THE SIMULATION */
 
 int main () {
 
@@ -245,6 +258,7 @@ int main () {
         // Use a std::vector because we might decide later to make more matrix
         // elements non-zero, e.g. for radiative decay.
         std::vector<matrix_element> matrix;
+        // TODO: Store just one row of the matrix in memory.
 
         for (cfg to = 0; to < n_configs; ++to) {
             // "to" should actually be "away" here.
@@ -263,6 +277,7 @@ int main () {
         weights guess = { 1 };  // Guess that the dot is in an empty config
                                 // to start with. The other elements get
                                 // initialised to 0.
+                                // TODO: Move this outside `for` loop.
         ddt_weights ratechange;
 
         bool converged = false;
